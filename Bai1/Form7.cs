d12f97fb3156de0224c31f2b2b2aea5346b163d5
justinluuvn <37363724+justinluuvn@users.noreply.github.com>
2020-01-26 13:51:03 +0200

    Changes

--- Bai1/Form7.cs
+++ Bai1/Form7.cs
@@ -26,8 +26,8 @@ namespace Bai1
             long x3 = x1 * temp0.Width / 873;
             long y3 = y1 * temp0.Height / 485;
             Bitmap bmap = (Bitmap)temp0.Clone();
-            long width = x3-x2, height = y3-y2;
-            Rectangle rect = new Rectangle(Convert.ToInt16(x2), Convert.ToInt16(y2), Convert.ToInt16(width), Convert.ToInt16(height));
+            long width = Math.Abs(x3-x2), height = Math.Abs(y3-y2);
+            Rectangle rect = new Rectangle(Convert.ToInt16((x2 > x3) ? x3 : x2), Convert.ToInt16((y2 > y3) ? y3 : y2), Convert.ToInt16(width), Convert.ToInt16(height));
             result = bmap.Clone(rect, bmap.PixelFormat);
             //result = a;
             this.Close();
@@ -42,15 +42,19 @@ namespace Bai1
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
+            int sx = (x > e.X) ? e.X : x;
+            int sy = (y > e.Y) ? e.Y : y;
+            int width = Math.Abs(x - e.X);
+            int height = Math.Abs(y - e.Y);
             if (crop == true)
             {
                 Bitmap temp = (Bitmap)temp1.Clone();
                 Graphics gr = Graphics.FromImage(temp);
                 Brush cBrush = new Pen(Color.FromArgb(150, Color.White)).Brush;
-                Rectangle rect1 = new Rectangle(0, 0, pictureBox1.Image.Width, y);
-                Rectangle rect2 = new Rectangle(0, y, x, Math.Abs(y - e.Y));
-                Rectangle rect3 = new Rectangle(0, e.Y, pictureBox1.Image.Width, pictureBox1.Image.Height);
-                Rectangle rect4 = new Rectangle(e.X, y, (pictureBox1.Image.Width - e.X), Math.Abs(y - e.Y));
+                Rectangle rect1 = new Rectangle(0, 0, pictureBox1.Image.Width, sy);
+                Rectangle rect2 = new Rectangle(0, sy, sx, height);
+                Rectangle rect3 = new Rectangle(0, sy + height, pictureBox1.Image.Width, pictureBox1.Image.Height);
+                Rectangle rect4 = new Rectangle(sx + width, sy, (pictureBox1.Image.Width - width - sx), height);
                 gr.FillRectangle(cBrush, rect1);
                 gr.FillRectangle(cBrush, rect2);
                 gr.FillRectangle(cBrush, rect3);
